e7590d3ab74dc6598dda4928a7c021fa52cac728
Leonardo Rodrigues <leonardojrr1@gmail.com>
2020-08-20 12:46:38 -0400

    React config

---
+++ build/precache-manifest.8e4bc0d6825122fb5be266a6a11fd955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d27d7ac29d0f6d50a7f124ef8a809a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9ed046ffc53dcec5120",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ddf13c0479785ef4885b",
+    "url": "/static/js/2.7b1fa290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b1fa290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ed046ffc53dcec5120",
+    "url": "/static/js/main.4a6a2c51.chunk.js"
+  },
+  {
+    "revision": "2afcd1f3819a90aa35c8",
+    "url": "/static/js/runtime-main.dc6ee7cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
